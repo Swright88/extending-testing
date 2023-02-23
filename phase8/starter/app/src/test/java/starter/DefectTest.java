8d79fcd39a3ffdf4f68ffbbf11c97147e272ff17
Edward Andress <eddie_andress@hotmail.com>
2023-02-23 11:54:15 +0000

    [SLUG] Transferring changes from Slug as of 2023-02-23 11:53:58 +0000.

--- phase8/starter/app/src/test/java/starter/DefectTest.java
+++ phase8/starter/app/src/test/java/starter/DefectTest.java
@@ -4,16 +4,21 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 // complete the test and dataprovider method
+// the goal is to practice parameterised testing
+// you don't need to use a spreadsheet (yet)
 
 public class DefectTest {
     @Test(dataProvider = "")
     public void isValidReturnsFalseForInvalidDefects(String name, String summary, String details) {
-        
+        System.out.println("Test case");
+        System.out.println("\tname: " + name);
+        System.out.println("\tsummary: " + summary);
+        System.out.println("\tdetails: " + details);
     }
 
     @DataProvider(name = "")
     public Object[][] invalidDefectData() {
-        // read from your spreadsheet
         // return a 2D array of inputs and expected urls
+        // you can hard code it for now
     }
 }
